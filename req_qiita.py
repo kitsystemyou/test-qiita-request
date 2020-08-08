e1f326cd51c14e3a54432b55d3652956aec5d860
singsignalyou <32862029+kitsystemyou@users.noreply.github.com>
2020-08-08 18:07:09 +0900

    add origin file

---
+++ req_qiita.py
@@ -0,0 +1,8 @@
+import requests
+import pprint
+
+with open('./q_token.txt') as f:
+    qiita_access_token = f.read().strip()
+
+header = {'Authorization': 'Bearer {}'.format(qiita_access_token)}
+print(header)
