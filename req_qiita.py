239cf9601d2b8701d612373fc7020809bdb09760
kitsystemyou <kitsystemyou@gmail.com>
2020-08-08 18:00:00 +0900

    Revert "conflict test"
    This reverts commit 0f7c8c06cc1a034c2a9199c97b7213789cd010c7.

--- req_qiita.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pprint
-
-with open('./q_token.txt') as f:
-    qiita_access_token = f.read().strip()
-
-header = {'Authorization': 'Bearer {}'.format(qiita_access_token)}
-print(header)
-print("conflict test")
