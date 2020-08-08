0f7c8c06cc1a034c2a9199c97b7213789cd010c7
kitsystemyou <kitsystemyou@gmail.com>
2020-08-08 17:50:55 +0900

    conflict test

---
+++ req_qiita.py
@@ -0,0 +1,9 @@
+import requests
+import pprint
+
+with open('./q_token.txt') as f:
+    qiita_access_token = f.read().strip()
+
+header = {'Authorization': 'Bearer {}'.format(qiita_access_token)}
+print(header)
+print("conflict test")
